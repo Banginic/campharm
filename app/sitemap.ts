291157612705313863b8e35041c2480582bc44b5
Banginic <borisndoh@gmail.com>
2025-08-15 15:08:48 +0100

    Fix sitemap url

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,7 +5,7 @@ import { desc } from "drizzle-orm";
 import { CAMEROON } from "@/assets/data";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = "http://localhost:3000";
+  const baseUrl = 'https://medyro.vercel.app' ;
 
   // 1️⃣ Static pages
   const staticRoutes: MetadataRoute.Sitemap = [
